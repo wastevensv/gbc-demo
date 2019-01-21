7ca7915adca0303a1281f1f9d9a9ced46380eff4
William Stevens <contact@wastevensv.com>
2019-01-21 05:28:00 -0500

    Prints hello world

--- Makefile
+++ Makefile
@@ -4,16 +4,16 @@ OBJS=$(patsubst src/%.asm, build/%.o, $(SRCS))
 
 .SECONDARY: $(OBJS)
 
-all: build/$(TARGET).gb
+all: build/$(TARGET).gbc
 
 build/%.o: src/%.asm
 	mkdir -p build/
-	rgbasm -i src/ -p 0xff -o $@ $^
+	rgbasm -i src/ -i data/ -p 0xff -o $@ $^
 
-build/%.gb: $(OBJS)
+build/%.gbc: $(OBJS)
 	mkdir -p build/
 	rgblink -p 0xff -n build/$*.sym -m build/$*.map -o $@ $(OBJS)
-	rgbfix -Cjv -i XXXX -k XX -l 0x33 -m 0x05 -p 0 -r 1 -t $(TARGET) $@
+	rgbfix -Cjv -i XXXX -k XX -l 0x33 -m 0x1A -r 0x04 -p 0 -r 1 -t $(TARGET) $@
 
 clean:
 	rm -r build/
--- src/demo.asm
+++ src/demo.asm
@@ -38,9 +38,90 @@ SECTION "bank0",ROM0[$61]
 
 SECTION "romheader",ROM0[$100]
     nop
-    jp Start150
+    jp Start
 
 SECTION "start",ROM0[$150]
 
-Start150:
+Start:
+    nop
+    di
+    ld sp, $fffe
+    ld a, $42
+
+init:
+; Reset pallete
+    ld a, %11100100
+    ld [rBGP], a
+    ld [rOBP0], a
+
+; Setup Pallette
+    ld a, %10000000
+    ld [$ff68], a
+
+    ld bc, %0111111111111111  ; white
+    ld a, c
+    ld [$ff69], a
+    ld a, b
+    ld [$ff69], a
+    ld bc, %0000001111100000  ; green
+    ld a, c
+    ld [$ff69], a
+    ld a, b
+    ld [$ff69], a
+    ld bc, %0000000000011111  ; red
+    ld a, c
+    ld [$ff69], a
+    ld a, b
+    ld [$ff69], a
+    ld bc, %0111110000000000  ; blue
+    ld a, c
+    ld [$ff69], a
+    ld a, b
+    ld [$ff69], a
+
+; Reset scrolling
+    ld a, 0
+    ld [rSCX], a
+    ld [rSCY], a
+
+; Turn off sound
+    ld [rNR52], a
+
+; Wait till the LCD is past VBlank
+.waitVBlank:
+    ld a, [rLY]
+    cp SCRN_Y
+    jr c, .waitVBlank
+
+    ld hl, _VRAM
+    ld de, FontTiles
+    ld bc, FontTilesEnd - FontTiles
+.copyFont
+    ld a, [de]    ; Grab 1 byte from the source
+    ld [hli], a   ; Place it at the destination, incrementing hl
+    inc de    	  ; Move to next byte
+    dec bc 	  ; Decrement count
+    ld a, b  	  ; Check if count is 0, since `dec bc` doesn't update flags
+    or c
+    jr nz, .copyFont
+
+    ld hl, _SCRN0+1 ; This will print the string at the top-left corner of the screen
+    ld de, HelloWorldStr
+.copyString
+    ld a, [de]
+    ld [hli], a
+    inc de
+    and a              ; Check if the byte we just copied is zero
+    jr nz, .copyString ; Continue if it's not
+
     halt
+
+SECTION "font", ROM0
+FontTiles:
+INCBIN "font.chr"
+FontTilesEnd:
+
+SECTION "strings", ROM0
+
+HelloWorldStr:
+    db "Hello World!", 0
